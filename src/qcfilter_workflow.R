6292dd9040a1c56468df8ff193a30f24fba95ca4
Sriram P C <sriram.pc@gmail.com>
2020-04-01 16:40:16 -0400

    update printing

--- src/qcfilter_workflow.R
+++ src/qcfilter_workflow.R
@@ -15,7 +15,7 @@ mcg_cell_filter = function(dfx){
     mcg_med = median(log_mcg)
     mcg_3madsp = mcg_med + 3 * mcg_mad
     mcg_threshold = min(5, 10^mcg_3madsp)
-    cat("\n MCG Upper bound :", mcg_threshold)
+    cat(mcg_threshold)
     (mcgpct > mcg_threshold)
 }
 
@@ -28,7 +28,7 @@ ngenes_cell_filter = function(dfx){
     ngenes_mad = mad(log_ngenes)
     ngenes_med = median(log_ngenes)
     ngenes_1mads = ngenes_med -  ngenes_mad
-    cat("\n Genes Lower bound :", 10^ngenes_1mads)
+    cat(10^ngenes_1mads)
     (log_ngenes < ngenes_1mads)
 }
 
@@ -41,24 +41,30 @@ avgcounts_cell_filter = function(dfx){
     lmean_mad = mad(log_libmean)
     lmean_med = median(log_libmean)
     lmean_1mads = lmean_med - lmean_mad
-    cat("\n Avg.cts Lower bound :", lmean_1mads, 10^lmean_1mads)
+    cat(10^lmean_1mads)
     (log_libmean < lmean_1mads)
 }
 
 
 apply_filters = function(out_dir, out_prefix, in_dirs){
+
+    cat("DIR", "NGENES", "NCELLS", 
+        "MCG_UB", "NGENES_LB", "AVGCTS_LB",
+        "MCG_CELLS", "MCG_PCT",
+        "NGENES_CELLS", "NGENES_PCT",
+        "AVGCTS_CELS", "AVGCTS_PCT",
+        "ALLF_CELLS", "ALLF_PCT", "\n")
     for(dirx in in_dirs){
         dfx = read10xCounts(dirx)
-        cat("Dims: ",  dirx, dim(dfx))
+        cat(dirx, dim(dfx)[1], dim(dfx)[2])
         # per.feat = perFeatureQCMetrics(dfx)
         mcg_drop = mcg_cell_filter(dfx)
         ngenes_drop = ngenes_cell_filter(dfx)
         avgcts_drop = avgcounts_cell_filter(dfx)
         nlength = dim(dfx)[2]
-        cat("\n MCG,", sum(mcg_drop), sum(mcg_drop)*100/nlength, 
-            "\n NGENES: ", sum(ngenes_drop), sum(ngenes_drop)*100/nlength, 
-            "\n AVGCTS: ", sum(avgcts_drop), sum(avgcts_drop)*100/nlength,
-            "\n ALL : ",
+        cat(sum(mcg_drop), sum(mcg_drop)*100/nlength, 
+            sum(ngenes_drop), sum(ngenes_drop)*100/nlength, 
+            sum(avgcts_drop), sum(avgcts_drop)*100/nlength,
             sum(mcg_drop | ngenes_drop | avgcts_drop),
             sum(mcg_drop | ngenes_drop | avgcts_drop)*100/nlength)
        cat("\n")
