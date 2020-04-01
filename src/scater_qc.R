f95e588670c7bf0454059f62df2b848f5a2dec39
Sriram P C <sriram.pc@gmail.com>
2020-04-01 18:14:55 -0400

    refactor scater qcs

--- src/scater_qc.R
+++ src/scater_qc.R
@@ -1,6 +1,52 @@
 source("seurat_plots.R")
 source("scater_plots.R")
 
+qcstat_dir = function(dirx){
+    dfx = read10xCounts(dirx)
+    per.cell = perCellQCMetrics(dfx, 
+        subset=list(MCG=grep("AT[MC]G",
+                        rownames(dfx))))
+    per.feat = perFeatureQCMetrics(dfx)
+    tfx = per.feat$detected * dim(dfx)[2]/100.0
+    libsize_mads = isOutlier(per.cell$sum, nmads=3,
+                                type="lower", log=FALSE)
+    genects_mads = isOutlier(per.cell$detected, nmads=3,
+                                type="lower", log=FALSE)
+    feat_cell_cts = per.feat$detected * dim(dfx)[2]/100.0
+    feat_cell_mean = per.feat$mean >= 1.0
+
+    libsize_95p = per.cell$sum < (quantile(per.cell$sum, .95) * 0.05)
+    genects_99p = per.cell$detected < (quantile(per.cell$detected, 0.99) * 0.05)
+
+    mcg_nmads = isOutlier(per.cell$subsets_MCG_percent, nmads=3, typ="higher")
+    mcg_5p = per.cell$subsets_MCG_percent > 5
+    mcg_4p = per.cell$subsets_MCG_percent > 4
+    mcg_3p = per.cell$subsets_MCG_percent > 3
+    c(#dirname = dirx,
+      total_cells = dim(dfx)[2],
+      libsize_mads=sum(libsize_mads),
+      genects_mads=sum(genects_mads),
+      libsize_95p=sum(libsize_95p),
+      genects_99p=sum(genects_99p),
+      mcg_nmads=sum(mcg_nmads),
+      mcg_5p=sum(mcg_5p),
+      mcg_4p=sum(mcg_4p),
+      mcg_3p=sum(mcg_3p),
+      total_features = dim(dfx)[1], 
+      feat_cell_mean=sum(feat_cell_mean)
+       )
+
+}
+
+get_qcstats = function(outfile, indirs){
+    print(outfile)
+    print(indirs)
+    qcl = lapply(indirs, qcstat_dir)
+    qdf = t(as.data.frame(qcl))
+    rownames(qdf) = indirs
+    write.table(qdf, outfile, sep="\t", quote=FALSE)
+}
+
 plot_flip_hist = function(per.cell, per.feat, dfx, dirx, out_dir, out_prefix){
     fname = paste(out_dir, dirx, paste(out_prefix, "-scater-qc-feat-filp-hist.png", sep=""), sep="/"  )
     plot_feat_counts_flip_hist(per.feat, dfx, dirx, fname)
@@ -62,6 +108,8 @@ plot_hist = function(per.cell, per.feat, dfx, dirx, out_dir, out_prefix){
 }
 
 plot_qcstats = function(out_dir, out_prefix, in_dirs){
+    outfile = paste(out_dir, "scater-qc.tsv", sep="/")
+    get_qcstats(outfile, indirs)
     for(dirx in in_dirs){
         dfx = read10xCounts(dirx)
         per.cell = perCellQCMetrics(dfx, 
